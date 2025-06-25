c823c7c16c6f8a1234b8f76cd3bc3d7dfdc12f51
Sean Evans <sean.w.evans@gmail.com>
2025-06-25 13:00:51 -0400

    Integrate BPF manager and cleanup

--- pyisolate/__init__.py
+++ pyisolate/__init__.py
@@ -3,11 +3,12 @@
 This module exposes the high-level API described in API.md.
 """
 
-from .supervisor import Supervisor, spawn, list_active, Sandbox
+from .supervisor import Supervisor, spawn, list_active, Sandbox, reload_policy
 
 __all__ = [
     "spawn",
     "list_active",
     "Sandbox",
     "Supervisor",
+    "reload_policy",
 ]
